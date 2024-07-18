b6d75c5a2250d259fb200a77e20811984109c3a8
Роман Бушуев <bushuev@benefitty.ru>
2024-07-18 14:35:11 +0400

    feat: api register & auth
    Sanctum регистрация и авторизация.

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 ```sh
  ./vendor/bin/sail up -d
 docker exec -it chat-back-laravel.test-1 sh -c "php artisan migrate"
+docker exec -it chat-back-laravel.test-1 sh -c "php artisan scribe:generate"
 ```
---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Knuckles\Scribe\Attributes\BodyParam;
+use Knuckles\Scribe\Attributes\Unauthenticated;
+use Symfony\Component\HttpFoundation\Response;
+
+#[Unauthenticated]
+class AuthController extends Controller
+{
+    #[BodyParam("password_confirmation", "string", example: 'No-example')]
+    public function register(RegisterUserRequest $data) {
+
+        $user = User::query()->create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], Response::HTTP_CREATED);
+    }
+
+    public function login(LoginUserRequest $data)
+    {
+        $user = User::query()->where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response([
+                'msg' => 'incorrect username or password'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ], Response::HTTP_CREATED);
+    }
+}
